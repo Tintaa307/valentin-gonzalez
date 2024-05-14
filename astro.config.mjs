918eca9ab109fae375863f9d781a603ce68d875e
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-05-13 23:01:24 -0300

    feat: add web analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+})
