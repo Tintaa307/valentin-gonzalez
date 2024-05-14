fc20a1b4eec5c0297478f673b08f138b4fccbda4
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-05-13 23:11:38 -0300

    feat: add vercel analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config"
 import vercel from "@astrojs/vercel/serverless"
-
 import tailwind from "@astrojs/tailwind"
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
